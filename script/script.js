c072076fa8e2c19d2882959d97c52a6750afb220
brrik <leo.luvs.footwork@gmail.com>
2022-05-31 13:58:30 +0900

    random jump added

--- script/script.js
+++ script/script.js
@@ -15,14 +15,17 @@
 //MainCTX.drawImage(canvas,0,0);
 
 
-const canvas = document.getElementById("MainC");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth - 30;
-canvas.height = window.innerHeight - 30;
+//　↑↑↑ここまでデバッグ用の処理のため、消しても大丈夫
 
-const enmRect = 50;
-let hitCount = 5;
 
+
+//定数の定義
+const loopTime = 1500; //モブがワープする時間間隔(ミリ秒：1000で1秒)
+const enmRect = 100; //敵モブの大きさ　初期設定100
+let hitCount = 5; //敵モブの体力　初期設定5
+
+
+//画像のファイル名　最初に読み込んでおくことで処理高速化と安定化
 const imageBG = new Image();
 imageBG.src = "./src/start.jpg";
 
@@ -31,6 +34,14 @@ imageEnm.src = "./src/enm1.jpg";
 
 
 
+//ゲーム画面の初期化
+const canvas = document.getElementById("MainC");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth - 30;
+canvas.height = window.innerHeight - 30;
+
+
+//最初のゲーム画面処理
 imageBG.addEventListener("load", function(){
     drawBG(canvas);
 });
@@ -40,18 +51,19 @@ imageEnm.addEventListener("load", function(){
     enmPoint = drawEnm(canvas);
 });
 
+
+
+//クリック時の判定
 canvas.addEventListener("click", e =>{
-    const canvas = document.getElementById("MainC");
-
-
     const rect = canvas.getBoundingClientRect();
     point = {
         x : e.clientX  - rect.left,
         y : e.clientY  - rect.top
     };
 
+    //ctx.fillStyle="rgb(255,0,0)";
+    //ctx.fillRect(point.x-25,point.y-25,50,50);
 
-    console.log(point);
 
     const hit = (enmPoint.x <= point.x && point.x <= enmPoint.w) && 
                 (enmPoint.y <= point.y && point.y <= enmPoint.h);
@@ -67,7 +79,7 @@ canvas.addEventListener("click", e =>{
             hitCount = 5;
         };
 
-        drawBG(canvas,"./src/start.jpg");
+        drawBG(canvas);
         enmPoint = drawEnm(canvas);
     }else{
         console.log("Miss");
@@ -77,16 +89,24 @@ canvas.addEventListener("click", e =>{
 });
 
 
+//一定時間ごとに繰り返す処理
+function mainProc(){
+    console.log("before mainProc EnmPoint");
+    console.log(enmPoint);
+    drawBG(canvas);
+    enmPoint = drawEnm(canvas);
+    console.log("after mainProc EnmPoint");
+    console.log(enmPoint);
+}
+
+//背景の処理
 function drawBG(canvas){
     console.log("***Draw Back Ground***");
     const ctx = canvas.getContext("2d");
     ctx.drawImage(imageBG, 0, 0, canvas.width, canvas.height);
 }
 
-
-
-
-
+//敵画像の処理
 function drawEnm(canvas){
     console.log("***Draw Enemy***");
 
@@ -95,10 +115,6 @@ function drawEnm(canvas){
 
     const ctx = canvas.getContext("2d");
     ctx.drawImage(imageEnm, enmX, enmY, enmRect, enmRect);
-    console.log("===ENM w/h===")
-    console.log(imageEnm.width);
-    console.log(imageEnm.height);
-    console.log("=============")
 
     const enmPath = {
         x : enmX,
@@ -110,3 +126,6 @@ function drawEnm(canvas){
     console.log(enmPath);
     return enmPath;
 }
+
+//モブの一定間隔ジャンプ処理
+setInterval(mainProc,1500);
