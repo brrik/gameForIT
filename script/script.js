0bff073d510243a3df8481b1642c297692f9ee93
BRRIK <leo.luvs.footwork@gmail.com>
2022-06-01 04:55:57 +0900

    added barrier and music

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
-//const MainCanvas = document.getElementById("MainC");
-//const MainCTX = MainCanvas.getContext("2d");
+//const MainbattleCanvas = document.getElementById("MainC");
+//const MainbtlCtx = MainbattleCanvas.getContext("2d");
 
-//MainCanvas.width = window.innerWidth - 30;
-//MainCanvas.height = window.innerHeight - 30;
+//MainbattleCanvas.width = window.innerWidth - 30;
+//MainbattleCanvas.height = window.innerHeight - 30;
 
 
 
-//const canvas = document.createElement("canvas");
-//const ctx = canvas.getContext("2d");
+//const battleCanvas = document.createElement("battleCanvas");
+//const btlCtx = battleCanvas.getContext("2d");
 
-//canvas.width = MainCanvas.width;
-//canvas.height = MainCanvas.height;
+//battleCanvas.width = MainbattleCanvas.width;
+//battleCanvas.height = MainbattleCanvas.height;
 
-//MainCTX.drawImage(canvas,0,0);
+//MainbtlCtx.drawImage(battleCanvas,0,0);
 
 
 //　↑↑↑ここまでデバッグ用の処理のため、消しても大丈夫
@@ -23,109 +23,178 @@
 const loopTime = 1500; //モブがワープする時間間隔(ミリ秒：1000で1秒)
 const enmRect = 100; //敵モブの大きさ　初期設定100
 let hitCount = 5; //敵モブの体力　初期設定5
+const enmBarrier = 3; //敵モブのバリア動作率　初期設定3　＝　1/3でバリア解除状態
+let barrier = 0; //バリア発動フラグ
 
 
-//画像のファイル名　最初に読み込んでおくことで処理高速化と安定化
+//画像読み込み用のバッファ　最初に読み込んでおくことで処理高速化と安定化
 const imageBG = new Image();
-imageBG.src = "./src/start.jpg";
-
 const imageEnm = new Image();
-imageEnm.src = "./src/enm1.jpg";
 
+//音楽読み込み用のバッファ　最初に読み込んでおくことで処理高速化と安定化
+const titleMusic = new Audio(""); titleMusic.loop = true;
+const minorBattleMusic = new Audio(""); minorBattleMusic.loop = true;
+const bossBattleMusic = new Audio(""); bossBattleMusic.loop = true;
 
 
 //ゲーム画面の初期化
+//複数の画面を仮想的に用意して、それをメインキャンバスにコピーすることで画面変遷を実装
+
+//画面描画用のメインキャンバス
 const canvas = document.getElementById("MainC");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth - 30;
 canvas.height = window.innerHeight - 30;
 
+//雑魚戦用キャンバス
+const battleCanvas = document.createElement("canvas");
+const btlCtx = battleCanvas.getContext("2d");
+battleCanvas.width = canvas.width;
+battleCanvas.height = canvas.height;
 
-//最初のゲーム画面処理
-imageBG.addEventListener("load", function(){
-    drawBG(canvas);
-});
-
-let enmPoint = {};
-imageEnm.addEventListener("load", function(){
-    enmPoint = drawEnm(canvas);
-});
-
-
-
-//クリック時の判定
-canvas.addEventListener("click", e =>{
-    const rect = canvas.getBoundingClientRect();
-    point = {
-        x : e.clientX  - rect.left,
-        y : e.clientY  - rect.top
-    };
-
-    //ctx.fillStyle="rgb(255,0,0)";
-    //ctx.fillRect(point.x-25,point.y-25,50,50);
+//動作検証用キャンバス
+const fillCanvas = document.createElement("canvas");
+const fillCtx = fillCanvas.getContext("2d");
+fillCanvas.width = canvas.width;
+fillCanvas.height = canvas.height;
 
 
-    const hit = (enmPoint.x <= point.x && point.x <= enmPoint.w) && 
-                (enmPoint.y <= point.y && point.y <= enmPoint.h);
+minorBattle("./src/enm1.jpg","./src/start.jpg");
 
-    console.log(hit)
 
-    if(hit){
-        console.log("HIT! Hit count is " + hitCount);
-        
-        hitCount = hitCount -1;
-        if(hitCount==0){
-            alert("Clear!!");
-            hitCount = 5;
+
+
+//雑魚戦時の処理
+function minorBattle(enmSrc, bgSrc){
+    imageEnm.src = enmSrc;
+    imageBG.src = bgSrc;
+
+    minorBattleMusic.play();
+    
+
+    //起動時の画面読み込み処理
+    imageBG.addEventListener("load", function(){
+        drawBG(battleCanvas);
+    });
+
+    let enmPoint = {};
+    imageEnm.addEventListener("load", function(){
+        enmPoint = drawEnm(battleCanvas);
+    });
+
+
+    let battleClear = false;
+
+
+    //クリック時の判定を追加
+    canvas.addEventListener("click", e =>{
+        const rect = canvas.getBoundingClientRect();
+        point = {
+            x : e.clientX  - rect.left,
+            y : e.clientY  - rect.top
         };
 
-        drawBG(canvas);
-        enmPoint = drawEnm(canvas);
-    }else{
-        console.log("Miss");
+        //btlCtx.fillStyle="rgb(255,0,0)";
+        //btlCtx.fillRect(point.x-25,point.y-25,50,50);
+
+        if(barrier <= 1){
+        const hit = (enmPoint.x <= point.x && point.x <= enmPoint.w) && 
+                    (enmPoint.y <= point.y && point.y <= enmPoint.h);
+
+        console.log(hit)
+
+        if(hit){
+            console.log("HIT! Hit count is " + hitCount);
+            
+            hitCount = hitCount -1;
+            if(hitCount==0){
+                alert("Clear!!");
+                hitCount = 5;
+                battleClear = true;
+            };
+
+            drawBG(battleCanvas);
+            enmPoint = drawEnm(battleCanvas);
+            ctx.drawImage(battleCanvas,0,0);
+        }else{
+            console.log("Miss");
+        }}else{
+            ctx.fillStyle="white";
+            ctx.font = '48px serif';
+            ctx.fillText('防御された！', enmPoint.x, enmPoint.y);
+            console.log("Barriered!!");
+        };
+        console.log("in event listener  x: " + point.x + "/ y: " + point.y);
+
+    });
+
+
+
+
+    //一定時間ごとに繰り返す処理
+    function mainProc(){
+        console.log("before mainProc EnmPoint");
+        console.log(enmPoint);
+        drawBG(battleCanvas);
+        enmPoint = drawEnm(battleCanvas);
+        console.log("after mainProc EnmPoint");
+        console.log(enmPoint);
+        console.log("copy screen");
+        ctx.drawImage(battleCanvas,0,0);
+        console.log(battleClear);
+        if(battleClear == true){
+            clearInterval(minorBattle);
+        }
     }
-    console.log("in event listener  x: " + point.x + "/ y: " + point.y);
-
-});
 
 
-//一定時間ごとに繰り返す処理
-function mainProc(){
-    console.log("before mainProc EnmPoint");
-    console.log(enmPoint);
-    drawBG(canvas);
-    enmPoint = drawEnm(canvas);
-    console.log("after mainProc EnmPoint");
-    console.log(enmPoint);
+
+
+    //背景の処理
+    function drawBG(battleCanvas){
+        console.log("***Draw Back Ground***");
+        const btlCtx = battleCanvas.getContext("2d");
+        btlCtx.drawImage(imageBG, 0, 0, battleCanvas.width, battleCanvas.height);
+    }
+
+    //敵画像の処理
+    function drawEnm(battleCanvas){
+        console.log("***Draw Enemy***");
+
+        barrier = Math.floor(Math.random() * enmBarrier);
+
+        let enmX = Math.floor(Math.random() * (battleCanvas.width - enmRect));
+        let enmY = Math.floor(Math.random() * (battleCanvas.height - enmRect));
+
+        const btlCtx = battleCanvas.getContext("2d");
+
+        if(barrier > 1){
+            btlCtx.fillStyle = "rgb(100,100,255)";
+            btlCtx.fillRect(enmX-10, enmY-10, enmRect+20, enmRect+20);
+
+        }
+
+        btlCtx.drawImage(imageEnm, enmX, enmY, enmRect, enmRect);
+
+        const enmPath = {
+            x : enmX,
+            y : enmY,
+            w : enmX + enmRect,
+            h : enmY + enmRect  
+        };
+        console.log("battleCanvas h:" + battleCanvas.height + " / battleCanvas w:" + battleCanvas.width);
+        console.log(enmPath);
+        return enmPath;
+    }
+
+
+    function fillCanv(){
+        fillCtx.fillStyle = "rgb(255,0,0)";
+        fillCtx.fillRect(0,0,canvas.width,canvas.height);
+    }
+
+
+
+    //モブの一定間隔ジャンプ処理
+    const minorBattle = setInterval(mainProc,1500);
 }
-
-//背景の処理
-function drawBG(canvas){
-    console.log("***Draw Back Ground***");
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(imageBG, 0, 0, canvas.width, canvas.height);
-}
-
-//敵画像の処理
-function drawEnm(canvas){
-    console.log("***Draw Enemy***");
-
-    let enmX = Math.floor(Math.random() * (canvas.width - enmRect));
-    let enmY = Math.floor(Math.random() * (canvas.height - enmRect));
-
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(imageEnm, enmX, enmY, enmRect, enmRect);
-
-    const enmPath = {
-        x : enmX,
-        y : enmY,
-        w : enmX + enmRect,
-        h : enmY + enmRect  
-    };
-    console.log("canvas h:" + canvas.height + " / canvas w:" + canvas.width);
-    console.log(enmPath);
-    return enmPath;
-}
-
-//モブの一定間隔ジャンプ処理
-setInterval(mainProc,1500);
